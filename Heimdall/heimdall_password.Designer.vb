7a09c9d8c18720a52f3a5714f37175d8751da4be
Gökhan <38686443+HBacker@users.noreply.github.com>
2021-05-20 12:16:46 +0300

    master_login

--- Heimdall/heimdall_password.Designer.vb
+++ Heimdall/heimdall_password.Designer.vb
@@ -23,115 +23,43 @@ Partial Class heimdall_password
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.FormSkin1 = New Global.Heimdall.FormSkin()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.GunaCirclePictureBox1 = New Guna.UI.WinForms.GunaCirclePictureBox()
-        Me.GunaLabel3 = New Guna.UI.WinForms.GunaLabel()
         Me.Víðarr = New System.ComponentModel.BackgroundWorker()
         Me.elipse_heimdall = New Guna.UI.WinForms.GunaElipse(Me.components)
-        Me.GunaElipsePanel1 = New Guna.UI.WinForms.GunaElipsePanel()
         Me.elipse_password = New Guna.UI.WinForms.GunaElipse(Me.components)
-        Me.FormSkin1.SuspendLayout()
-        CType(Me.GunaCirclePictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.master_login = New Global.Heimdall.login()
         Me.SuspendLayout()
-        '
-        'FormSkin1
-        '
-        Me.FormSkin1.BackColor = System.Drawing.Color.White
-        Me.FormSkin1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(7, Byte), Integer), CType(CType(7, Byte), Integer), CType(CType(29, Byte), Integer))
-        Me.FormSkin1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(7, Byte), Integer), CType(CType(7, Byte), Integer), CType(CType(29, Byte), Integer))
-        Me.FormSkin1.Controls.Add(Me.Label1)
-        Me.FormSkin1.Controls.Add(Me.GunaCirclePictureBox1)
-        Me.FormSkin1.Controls.Add(Me.GunaLabel3)
-        Me.FormSkin1.Dock = System.Windows.Forms.DockStyle.Top
-        Me.FormSkin1.FlatColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
-        Me.FormSkin1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
-        Me.FormSkin1.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(6, Byte), Integer), CType(CType(16, Byte), Integer), CType(CType(27, Byte), Integer))
-        Me.FormSkin1.HeaderMaximize = False
-        Me.FormSkin1.Location = New System.Drawing.Point(0, 0)
-        Me.FormSkin1.Name = "FormSkin1"
-        Me.FormSkin1.Size = New System.Drawing.Size(358, 52)
-        Me.FormSkin1.TabIndex = 2
-        Me.FormSkin1.Text = "FormSkin1"
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.BackColor = System.Drawing.Color.Transparent
-        Me.Label1.Font = New System.Drawing.Font("Nirmala UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.DarkSlateGray
-        Me.Label1.Location = New System.Drawing.Point(195, -1)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(57, 15)
-        Me.Label1.TabIndex = 3
-        Me.Label1.Text = "password"
-        '
-        'GunaCirclePictureBox1
-        '
-        Me.GunaCirclePictureBox1.BackColor = System.Drawing.Color.Transparent
-        Me.GunaCirclePictureBox1.BaseColor = System.Drawing.Color.Transparent
-        Me.GunaCirclePictureBox1.Image = Global.Heimdall.My.Resources.Resources.icons8_eye_64
-        Me.GunaCirclePictureBox1.Location = New System.Drawing.Point(96, 9)
-        Me.GunaCirclePictureBox1.Name = "GunaCirclePictureBox1"
-        Me.GunaCirclePictureBox1.Size = New System.Drawing.Size(35, 35)
-        Me.GunaCirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.GunaCirclePictureBox1.TabIndex = 1
-        Me.GunaCirclePictureBox1.TabStop = False
-        Me.GunaCirclePictureBox1.UseTransfarantBackground = False
-        '
-        'GunaLabel3
-        '
-        Me.GunaLabel3.AutoSize = True
-        Me.GunaLabel3.BackColor = System.Drawing.Color.Transparent
-        Me.GunaLabel3.Font = New System.Drawing.Font("Trebuchet MS", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
-        Me.GunaLabel3.ForeColor = System.Drawing.Color.White
-        Me.GunaLabel3.Location = New System.Drawing.Point(137, 9)
-        Me.GunaLabel3.Name = "GunaLabel3"
-        Me.GunaLabel3.Size = New System.Drawing.Size(123, 35)
-        Me.GunaLabel3.TabIndex = 2
-        Me.GunaLabel3.Text = "Heimdall"
         '
         'elipse_heimdall
         '
         Me.elipse_heimdall.Radius = 5
         Me.elipse_heimdall.TargetControl = Me
         '
-        'GunaElipsePanel1
-        '
-        Me.GunaElipsePanel1.BackColor = System.Drawing.Color.Transparent
-        Me.GunaElipsePanel1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(6, Byte), Integer), CType(CType(16, Byte), Integer), CType(CType(27, Byte), Integer))
-        Me.GunaElipsePanel1.Location = New System.Drawing.Point(-4, 50)
-        Me.GunaElipsePanel1.Name = "GunaElipsePanel1"
-        Me.GunaElipsePanel1.Size = New System.Drawing.Size(369, 166)
-        Me.GunaElipsePanel1.TabIndex = 3
-        '
         'elipse_password
         '
         Me.elipse_password.Radius = 3
+        Me.elipse_password.TargetControl = Me
+        '
+        'master_login
+        '
+        Me.master_login.Location = New System.Drawing.Point(0, 0)
+        Me.master_login.Name = "master_login"
+        Me.master_login.Size = New System.Drawing.Size(348, 215)
+        Me.master_login.TabIndex = 0
         '
         'heimdall_password
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(358, 215)
-        Me.Controls.Add(Me.FormSkin1)
-        Me.Controls.Add(Me.GunaElipsePanel1)
+        Me.ClientSize = New System.Drawing.Size(348, 215)
+        Me.Controls.Add(Me.master_login)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "heimdall_password"
         Me.Text = "heimdall_password"
-        Me.FormSkin1.ResumeLayout(False)
-        Me.FormSkin1.PerformLayout()
-        CType(Me.GunaCirclePictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents FormSkin1 As Global.Heimdall.FormSkin
-    Friend WithEvents GunaCirclePictureBox1 As Guna.UI.WinForms.GunaCirclePictureBox
-    Friend WithEvents GunaLabel3 As Guna.UI.WinForms.GunaLabel
     Friend WithEvents Víðarr As System.ComponentModel.BackgroundWorker
     Friend WithEvents elipse_heimdall As Guna.UI.WinForms.GunaElipse
-    Friend WithEvents GunaElipsePanel1 As Guna.UI.WinForms.GunaElipsePanel
-    Friend WithEvents Label1 As Label
     Friend WithEvents elipse_password As Guna.UI.WinForms.GunaElipse
+    Friend WithEvents master_login As Global.Heimdall.login
 End Class
